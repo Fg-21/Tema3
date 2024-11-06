ecccbdd91996b1801808e56c88144f53d64a08a0
Fg-21 <fm.gonzalez@iesnervion.es>
2024-11-06 14:41:24 +0100

    5 done

--- Tema3/src/boletinarrays/E5.java
+++ Tema3/src/boletinarrays/E5.java
@@ -13,15 +13,36 @@ public class E5 {
 		//Creamos la tabla para los números a preguntar al usuario
 		double arr[] = new double[10];
 		
+		//Creamos una variable para guardar el máximo
+		double max = 0;
+		
+		//Creamos una variable para guardar el mínimo
+		double min = Integer.MAX_VALUE;
+		
+		//Creamos una variable para sumar los números de la tabla
+		double suma = 0;
+		
 		//Abrimos scanner
 		Scanner rd = new Scanner(System.in);
 		
+		
 		//Preguntamos al usuario los números y los asignamos a la tabla
-		for (int i = 0; i <= arr.length; i++) {
+		for (int i = 0; i <= arr.length -1; i++) {
 			System.out.println("Introduce el número para la posición " + i);
 			arr[i] = rd.nextDouble();
 		}
 		
+		//Sumamos todos los números y vemos quien es el mayor
+		for (double value : arr) {
+			suma += value;
+			max =Math.max(max, value);
+			min =Math.min(min, value);
+		}
 		
+		
+		//Imprimimos la suma de los valores y el valor máximo y el mínimo
+		System.out.println("La suma de los valores es: " + suma);
+		System.out.println("El valor más alto es: " + max);
+		System.out.println("El valor más bajo es: " + min);
 	}
 }

6e5bc12b07aaf574e1bcadf3a4527076df9198fd
Fg-21 <fm.gonzalez@iesnervion.es>
2024-12-11 00:19:47 +0100

    3 DONE & 4 half

--- Tema3/src/boletinstrings/E3.java
+++ Tema3/src/boletinstrings/E3.java
@@ -1,5 +1,7 @@
 package boletinstrings;
 
+import java.util.Scanner;
+
 public class E3 {
 	public static void main(String[] args) {
 		/*
@@ -13,14 +15,39 @@ public class E3 {
 		//Variable para la cantidad de espacios en blanco
 		int spaces;
 		
+		//Abrimos scanner
+		Scanner rd = new Scanner(System.in);
+		
+		//Pedimos al usuario la frase
+		System.out.println("Escribe la frase para contar sus espacios:");
+		frase = rd.nextLine();
+		
 		//Llamamos a la función
 		spaces = spacesCount(frase);
+		
+		//Decimos cuantos espacios hay en la frase
+		System.out.println("En tu frase hay " + spaces + " espacios");
+		
+		//Cerramos scanner 
+		rd.close();
 		
 		
 	}
 	
 	//
 	static int spacesCount(String frase) {
-		//
+		//Variable para el número de espacios
+		int spaces = 0;
+		
+		//Bucle hasta la longitud de la frase
+		for (int i = 0; i < frase.length(); i++) {
+			//Si hay espacio, sumamos uno a spaces
+			if (frase.charAt(i) == ' ') {
+				spaces++;
+			}
+		}
+		
+		//Devolvemos el número de espacios
+		return spaces;
 	}
 }

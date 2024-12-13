ea2240e31d2c82de176af5a42a11804478c246cc
Fg-21 <fm.gonzalez@iesnervion.es>
2024-12-13 14:42:12 +0100

    9&10

---
+++ Tema3/src/boletinstrings/E10.java
@@ -0,0 +1,75 @@
+package boletinstrings;
+
+import java.util.Scanner;
+
+public class E10 {
+	public static void main(String[] args) {
+		/*
+		 * Disponemos de la siguiente relación de letras: 
+		 * conjunto 1: e i k m p q r s t u v 
+		 * conjunto 2: p v i u m t e r k q s
+		 * 
+		 * mediante la cual es posible codificar un texto, convirtiendo cada letra del
+		 * conjunto 1, en su correspondiente del conjunto 2. El resto de las letras no
+		 * se modifican. Los conjuntos se utilizan tanto para codificar mayúsculas como
+		 * minúsculas, mostrando siempre la codificación en minúsculas. Un ejemplo: la
+		 * palabra «PaquiTo» se codifica como «matqvko». Se pide realizar un programa
+		 * codificador, en el que se implemente la función: char codifica(char
+		 * conjunto1[],char conjunto2[], char c) que devuelve el carácter c codificado
+		 * según los conjuntos 1 y 2 que se le pasan.
+		 */
+		
+		//Variable para guardar el texto
+		String text;
+		
+		//Variable para guardar el texto codificado
+		String codifiedText = "";
+		
+		//Variable para el conjunto 1
+		char conjunto1 [] = {'e', 'i', 'k', 'm', 'p', 'q', 'r', 's', 't','u' ,'v'};
+		
+		//Variable para el conjunto 2
+		char conjunto2 [] = {'p', 'v', 'i', 'u', 'm', 't', 'e', 'r', 'k', 'q', 's'};
+		
+		//Variable para guardar el carácter a codificar
+		char c;
+		
+		//Abrimos scanner
+		Scanner rd = new Scanner(System.in);
+		
+		//Pedimos al usuario el texto
+		System.out.println("Introduce el texto a codificar");
+		text = rd.nextLine();
+		
+		//Pasamos el texto a minúsculas
+		
+		
+		//Recorremos el texto y enviamos a la función los caracteres individuales
+		for (int i = 0; i < text.length(); i++) {
+			c = text.charAt(i);
+			codifiedText += encrypter(conjunto1, conjunto2, c);
+			
+		}
+		
+		System.out.println(codifiedText);
+	}
+	
+	//Método para encriptar
+	static char encrypter(char[] conjunto1, char[] conjunto2, char c) {
+		//Variable para guardar el caracter encriptado
+		char encryptedChar = ' ';
+		
+		//Recorremos el conjunto 1 para ver si nuestro caracter se puede encriptar
+		for (int i = 0; i < conjunto1.length; i++) {
+			//Si coinciden los caracteres, se traduce al del conjunto 2
+			if(c == conjunto1[i]) {
+				encryptedChar = conjunto2[i];
+			} 
+		}
+		
+		
+		//Devolvemos un caracter ya encriptado
+		return encryptedChar;
+	}
+	
+}
--- Tema3/src/boletinstrings/E9.java
+++ Tema3/src/boletinstrings/E9.java
@@ -1,5 +1,7 @@
 package boletinstrings;
 
+import java.util.Scanner;
+
 public class E9 {
 	public static void main(String[] args) {
 		/*
@@ -14,6 +16,72 @@ public class E9 {
 		 * dialectos), y en caso afirmativo, nos muestre solo el mensaje sin muletillas.
 		 */
 		
+		//Variable para la frase introducida
+		String frase;
 		
+		//Variable para la frase traducida
+		String translated;
+		
+		//Variables para saber si es javalin o javalen
+		boolean javalinlen = false;
+		
+		//Abrimos scanner
+		Scanner rd = new Scanner(System.in);
+		
+		//Pedimos al usuario la frase
+		System.out.println("Introduce la frase:");
+		frase = rd.nextLine();
+		
+		//Comprobamos si es de Javalandia
+		if (frase.startsWith("Javalín, javalón")) {		
+			//Informamos de que es un dialecto
+			System.out.println("Es el dialecto javalén");
+			
+			//Ponemos javalínlen a positivo para indicar que el dialecto es Javalín, Javalón
+			javalinlen = true;
+	
+			//Llamamos al método de traducir e imprimimos la frase sin el dialecto
+			translated = translate(frase, javalinlen);
+			System.out.println(translated);
+			
+		} else if (frase.endsWith("javalén, len, len")){
+			//Informamos de que es un dialecto
+			System.out.println("Es el dialecto javalén");
+			
+			//Llamamos al método de traducir e imprimimos la frase sin el dialecto
+			translated = translate(frase, javalinlen);
+			System.out.println(translated);
+		}else if (frase.startsWith("Javalín, javalón") && frase.endsWith("javalén, len, len")) {
+			System.out.println("No se comprenden 2 dialectos a la vez");
+		} else {
+			System.out.println("La frase no es de un dialecto de Javalandia");
+		}
+		
+		//Cerramos scanner
+		rd.close();
+		
+	}
+	
+	//Método para traducir
+	static String translate(String frase, boolean javalinlen) {
+		//Variable para la frase traducida
+		String traducción;
+		
+		if (javalinlen) {
+			//Substraemos javalin javalon
+			traducción = frase.substring(17, frase.length());
+			
+			//Quitamos los espacios al inicio y al final
+			traducción = traducción.trim();
+		} else {
+			//Substraemos javalen len len 
+			traducción = frase.substring(0, frase.length()-17);	
+			
+			//Quitamos los espacios al inicio y al final
+			traducción = traducción.trim();
+		}
+	
+		//Devolvemos la traducción
+		return traducción;
 	}
 }

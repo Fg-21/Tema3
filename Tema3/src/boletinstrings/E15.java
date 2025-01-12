9aef2f438eea53620345bb85f49484ac712af9dc
Fg-21 <fm.gonzalez@iesnervion.es>
2025-01-12 20:54:24 +0100

    DONE

--- Tema3/src/boletinstrings/E15.java
+++ Tema3/src/boletinstrings/E15.java
@@ -1,3 +1,5 @@
+package boletinstrings;
+
 import java.util.Random;
 import java.util.Scanner;
 
---
+++ Tema3/src/boletinstrings/E17.java
@@ -0,0 +1,47 @@
+package boletinstrings;
+
+
+public class E17 {
+
+	public static void main(String[] args) {
+		System.out.println(eliminarComentariosC("Entrada: if (a==3) /* igual a tres */ a++; /* incrementamos a */ Salida: if (a==3) a++;"));
+	}
+	
+	public static String eliminarComentariosC(String codigo) {
+		// Creo una variable para almacenar la repuesta
+		String res = "";
+		
+		// Utilizo este regex que me elimina los comentarios
+		final String regex = "(?<!\\\"|\\')((/\\*([^*]|[\\r\\n]|(\\*+([^*/]|[\\r\\n])))*\\*+/)|(//.*))(?!\\\"|\\')";
+		
+		// Creo un array para separar el código sin comentarios
+		String[] codigoSeparado; 
+		codigoSeparado = codigo.split(regex);
+		
+		// Voy separa
+		for (int i = 0 ; i < codigoSeparado.length ; i++) {
+			res += codigoSeparado[i].trim() + "\n"; 
+		}
+		
+		return res;
+	}
+	
+	public static String eliminarComentariosCSubString(String sentenciaC) {
+		// Creo una variable para almacenar la repuesta
+		String res = "";
+		
+		final String INICIO = "/*";
+		final String FIN = "*/";
+		int posInicio = sentenciaC.indexOf(INICIO);
+		int posFin = 0;
+		
+		while (posInicio >= 0) {
+			res += sentenciaC.substring(posFin,posInicio);
+			posFin = sentenciaC.indexOf(FIN, posInicio) + 2;
+			posInicio = sentenciaC.indexOf(INICIO, posFin);
+		}
+		res += sentenciaC.substring(posFin, sentenciaC.length());
+		return res;
+	}
+	
+}
